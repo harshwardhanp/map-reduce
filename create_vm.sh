959b940d49de3b0a4269b1548068ba3a38d3dd73
Harshwardhan Raghunath Patil <43771203+harshwardhanp@users.noreply.github.com>
2022-11-30 23:27:45 -0500

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -10,16 +10,6 @@ gcloud compute instances create $MY_INSTANCE_NAME \
     --zone $ZONE \
     --network-interface=network-tier=PREMIUM,subnet=$SUBNET_NAME
 
-# new instance create script
-# gcloud compute instances create $MY_INSTANCE_NAME \
-#     --network-interface=network-tier=PREMIUM,subnet=$subnet_name \
-#     --metadata-from-file startup-script=startup-script.sh \
-#     --zone $ZONE \
-#     --tags http-server
-
+gcloud compute scp keystore.json $USER@$1:/home/$USER --zone=us-west1-a
 
 gcloud compute instances describe $1 --zone=us-west1-a --format='get(networkInterfaces[0].networkIP)'
-
-
-# gcloud compute instances create server-vm --metadata-from-file startup-script=startup-script.sh --zone=us-central1-a --network-interface=network-tier=PREMIUM,subnet=harsh-ecc-fall22
-# gcloud compute instances create client-vm --metadata-from-file startup-script=startup-script.sh --zone=us-central1-a --network-interface=network-tier=PREMIUM,subnet=harsh-ecc-fall22
